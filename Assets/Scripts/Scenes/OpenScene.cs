486483c289b8c76395e754bae24a758e82acd4d8
Dmitry-Stukalov <dimastukalov04@gmail.com>
2025-04-16 19:21:46 +0300

    Add open scene script

---
+++ Assets/Scripts/Scenes/OpenScene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class OpenScene : MonoBehaviour, IPointerClickHandler
+{
+	[field: SerializeField] private string SceneName;
+
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		StartCoroutine(LoadScene(SceneName));
+	}
+
+	private IEnumerator LoadScene(string scenename)
+	{
+		AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(scenename);
+
+		while (!asyncLoad.isDone)
+		{
+			yield return null;
+		}
+	}
+}
